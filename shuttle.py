2ed77e86074ef02311277dc0fb21991aa2476026
adsqueiroz <adsvrsqueiroz@gmail.com>
2021-11-18 00:26:01 -0300

    updates!

--- shuttle.py
+++ shuttle.py
@@ -3,7 +3,7 @@
 import json
 import os
 
-with open("shuttle_0.json", "r") as file:
+with open("shuttle.json", "r") as file:
 	lines = file.read()
 
 x = json.loads(lines)
@@ -12,27 +12,28 @@ print("+------------------------------------------------------------------------
 print("| number | name                                        | command")                                             
 print("+-----------------------------------------------------------------------------------------------") 
 
+hosts = []
 count = 0
 
 for i in x["hosts"][0]["home"]:
     count = count + 1
     print("| {0}      | {1}   ".format(count, i["name"], i["cmd"]))
     print("|------------------------------------------------------ {0}".format(i["cmd"]))
+    hosts.append(i["cmd"])
 
+user_choice = int(input("\nEnter a bookmark to connect: "))
 
-op = int(input("\nEnter a bookmark to connect: "))
+if user_choice == 1:
+    os.system(hosts[0])
 
-if op == 1:
-    print(" ")
-    os.system(i["cmd"])        
+elif user_choice == 2:
+    os.system(hosts[1])
 
-elif op == 2:
-    print(" ")    
-    os.system(i["cmd"])        
+elif user_choice == 3:
+    os.system(hosts[2])
 
-elif op == 3:
-    print(" ")    
-    os.system(i["cmd"])        
+elif user_choice == 4:
+    os.system(hosts[3])
 
-else:
-    print("\nChoose again!")
+else: 
+    print("Try again!")
